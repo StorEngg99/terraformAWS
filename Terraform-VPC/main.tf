d7b29ee7c895bc77df30db58256a49dca01a057b
StorEngg <mahesh@aretasystems.com>
2018-06-08 06:17:12 +0100

    Added Terraform VPC Template 1 Private & 1 Public Subnet

---
+++ Terraform-VPC/main.tf
@@ -0,0 +1,81 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_vpc" "vpc" {
+ cidr_block   = "10.0.0.0/16"
+ enable_dns_support = true
+ enable_dns_hostnames = true
+
+  tags {
+      label = "Terraform VPC"
+     }
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = false
+
+  tags {
+    Name = "Terraform VPC"
+  }
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+
+  tags {
+    Name = "Terraform VPC"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+resource "aws_route_table" "public_routetable" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags {
+    label = "Terraform VPC"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_a" {
+  subnet_id      = "${aws_subnet.public_subnet_a.id}"
+  route_table_id = "${aws_route_table.public_routetable.id}"
+}
+resource "aws_eip" "nat_ip" {
+  vpc = true
+depends_on = ["aws_internet_gateway.igw"]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat_ip.id}"
+  subnet_id     = "${aws_subnet.public_subnet_a.id}"
+}
+
+resource "aws_route_table" "private_routetable" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat.id}"
+  }
+
+  tags {
+    label = "Terraform VPC"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_a" {
+  subnet_id      = "${aws_subnet.private_subnet_a.id}"
+  route_table_id = "${aws_route_table.private_routetable.id}"
+}
